ebdca97d511abd5ee9e35851eac56de7d5939f93
Curtis C <curtis.chat.hk@gmail.com>
2018-10-13 12:13:03 -0400

    loops

---
+++ pacman-for-loop.js
@@ -0,0 +1,13 @@
+// for (<<initializer>>; <<condition>>; <<post iteration>>) {
+//   <<statement>>
+// }
+
+//pacman-for-loop.js
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+for (var index = 0; index < ghosts.length; index++) {
+  console.log('Pac-Man eats ' + ghosts[index]);
+}
+
+// index ++ is for increment increase of 1
+// index -- for decrement reduction by 1
--- pacman-objects-2.js
+++ pacman-objects-2.js
@@ -1,3 +1,15 @@
 // pacman-objects-2.js
 var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
-console
+
+console.log(ghosts);
+// dot-notation
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+// square bracket notation
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);
+
+//square bracket notation is used when the property you want to retrieve on an object is stored in a variable:
+
+var ghostName = 'clyde';
+console.log(ghosts[ghostName]);
