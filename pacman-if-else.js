8200a8cdf167047016d12ccab69431933483be59
Curtis C <curtis.chat.hk@gmail.com>
2018-10-13 12:50:50 -0400

    .

--- pacman-if-else.js
+++ pacman-if-else.js
@@ -1,22 +1,3 @@
-// # pacman.rb
-// power_pellet_eaten = false
-// ghosts = 4
-// while true do
-//   puts "Ghosts remaining: #{ghosts}"
-//
-//   if power_pellet_eaten == false
-//     puts 'Pac-Man eats the power pellet.'
-//     power_pellet_eaten = true
-//   elsif ghosts > 0
-//     puts 'Pac-Man eats a ghost.'
-//     ghosts--
-//   else
-//     break
-//   end
-// end
-//
-// puts 'Pac-Man goes home after a long day in the maze.'
-
 // pacman-if-else
 var powerPelletEaten = false;
 var ghosts = 4;
@@ -36,3 +17,22 @@ while (true) {
 }
 
 console.log('Pac-Man returns home after a long day in the maze');
+
+// # pacman.rb
+// power_pellet_eaten = false
+// ghosts = 4
+// while true do
+//   puts "Ghosts remaining: #{ghosts}"
+//
+//   if power_pellet_eaten == false
+//     puts 'Pac-Man eats the power pellet.'
+//     power_pellet_eaten = true
+//   elsif ghosts > 0
+//     puts 'Pac-Man eats a ghost.'
+//     ghosts--
+//   else
+//     break
+//   end
+// end
+//
+// puts 'Pac-Man goes home after a long day in the maze.'
