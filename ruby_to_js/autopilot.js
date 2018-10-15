ab1479999ab71134443c660f397c5fb57156d1bd
Curtis C <curtis.chat.hk@gmail.com>
2018-10-15 00:29:41 -0400

    converted autopilot.rb program

--- ruby_to_js/autopilot.js
+++ ruby_to_js/autopilot.js
@@ -7,7 +7,7 @@ function getNewCar() {
   return newCar;
 }
 
-function addCar(cars, new_car) {
+function addCar(cars, newCar) {
   cars.push(newCar);
   return "Added a new fleet to the car. Fleet is now " + cars.length + " cars.";
 }
@@ -19,5 +19,75 @@ function pickUpPassenger(car) {
 }
 
 function getDestination(car) {
-  if (car.city === 'Toronto')
+  if (car.city == 'Toronto') {
+    return 'Mississauga';
+  } else if(car.city == 'Mississauga') {
+    return 'London';
+  } else if(car.city == 'London') {
+    return 'Toronto'
+  }
 }
+
+function fillUpGas(car) {
+  var oldGas = car.gas;
+  car.gas = 100;
+  return "Filled up to " + getGasDisplay(car.gas) + " on gas from " + getGasDisplay(oldGas) + ".";
+}
+
+function getGasDisplay(gasAmount) {
+  return gasAmount + "%";
+}
+
+function drive(car, cityDistance) {
+  if (car.gas < cityDistance) {
+    return fillUpGas(car);
+  }
+
+  car.city = getDestination(car);
+  car.gas -= cityDistance;
+
+  return "Drove to " + car.city + ". Remaining gas: " + getGasDisplay(car.gas) + ".";
+}
+
+function dropOffPassengers(car) {
+  var previousPassengers = car.passengers;
+  car.passengers = 0;
+  return "Dropped off " + previousPassengers + " passengers."
+}
+
+function act(car) {
+  var distanceBetweenCities = 50;
+
+  if(car.gas < 20) {
+    return fillUpGas(car);
+  } else if(car.passenger < 3) {
+    return pickUpPassenger(car);
+  } else {
+    if(car.gas < distanceBetweenCities) {
+      return fillUpGas(car);
+    }
+    var droveTo = drive(car, distanceBetweenCities);
+    var passengersDropped = dropOffPassengers(car);
+
+    return droveTo + passengersDropped;
+  }
+}
+
+function commandFleet(cars) {
+  for(var i = 0; i < cars.length; i++) {
+    var action = act(cars[i]);
+    console.log("Car " + (i+1) + ": " + action);
+  }
+  console.log("---");
+}
+
+function addOneCarPerDay(cars, numDays) {
+  for(var i = 0; i < numDays; i ++) {
+    var newCar = getNewCar();
+    console.log(addCar(cars, newCar));
+    commandFleet(cars);
+  }
+}
+
+var cars = [];
+addOneCarPerDay(cars, 10)
