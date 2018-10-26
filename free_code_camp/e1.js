af2d1c4612e47c2ff0c865d073525b7acdf2f588
Curtis C <curtis.chat.hk@gmail.com>
2018-10-26 19:14:32 -0400

    worked on a few problems

--- free_code_camp/e1.js
+++ free_code_camp/e1.js
@@ -1,36 +1,13 @@
-// Setup
-var testObj = {
-  12: "Namath",
-  16: "Montana",
-  19: "Unitas"
-};
+// while loops
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+	ourArray.push(i);
+	i++;
+}
 
-// Only change code below this line;
-
-var playerNumber = 16;       // Change this Line
-var player = testObj[playerNumber];   // Change this Line
-
-=================================
-
-// Example
-var ourDog = {
-  "name": "Camper",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["everything!"]
-};
-
-ourDog.name = "Happy Camper";
-
-// Setup
-var myDog = {
-  "name": "Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
-
-// Only change code below this line.
-
-myDog.name = "Happy Coder";
-console.log(myDog);
+// for loops
+var myArray = [];
+for (var i=1; i < 6; i++) {
+	myArray.push(i);
+}
